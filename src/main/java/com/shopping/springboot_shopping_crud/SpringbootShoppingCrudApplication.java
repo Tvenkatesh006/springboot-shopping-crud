982321d98bebb822b2996401ee94efe5bf5dd0a0
Tvenkatesh006 <venkatesh200611@gmail.com>
2026-03-13 18:58:36 +0530

    Intial Spring Boot Project

---
+++ src/main/java/com/shopping/springboot_shopping_crud/SpringbootShoppingCrudApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.springboot_shopping_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShoppingCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootShoppingCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopping/springboot_shopping_crud/SpringbootShoppingCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.springboot_shopping_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShoppingCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
